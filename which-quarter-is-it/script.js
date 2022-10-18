53d3037ab5636ed103398cc90184746def6ff894
charles.hanna <charles.hanna@lightspeedhq.com>
2022-10-18 13:50:55 -0400

    which quarter is it

--- which-quarter-is-it/script.js
+++ which-quarter-is-it/script.js
@@ -1,27 +1,42 @@
-function checkLastQuarterProjects(q){
+function getCurrentQuarter(){
   const year = new Date().getFullYear();
   const q1 = {start: `${year}/4/01`, end: `${year}/6/30`};
   const q2 = {start: `${year}/7/01`, end: `${year}/9/30`};
   const q3 = {start: `${year}/10/01`, end: `${year}/12/31`};
   const q4 = {start: `${year+1}/1/1`, end: `${year+1}/3/31`};
   const quarterObject = {q1, q2, q3, q4};
-  if(!q){
-    const date = new Date();
-    const fullDate = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`;
-    const currentTimestamp = new Date(fullDate).getTime();
-    for (const [key, value] of Object.entries(quarterObject)) {
-      const quarterEndTimestamp = new Date(value.end).getTime();
-      if(currentTimestamp > quarterEndTimestamp){
-        q = key;
+
+  const date = new Date();
+  const fullDate = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`;
+  const currentTimestamp = new Date(fullDate).getTime();
+  const quarterMap = {
+    q1: 'First Quarter',
+    q2: 'Second Quarter',
+    q3: 'Third Quarter',
+    q4: 'Fourth Quarter'
+  }
+  for (const [key, value] of Object.entries(quarterObject)) {
+    const quarterEndTimestamp = new Date(value.end).getTime();
+    const quarterStartTimestamp = new Date(value.start).getTime();
+    if(currentTimestamp < quarterEndTimestamp && currentTimestamp >= quarterStartTimestamp){
+      return {
+        quarter: quarterMap[key],
+        startDate: value.start,
+        endDate: value.end,
+        todayDate: fullDate
       }
     }
   }
-
-  const query = `project = MW AND Sprint in (closedSprints()) AND status = Done AND assignee = currentUser() AND updated >= "${quarterObject[q].start}" AND updated <= "${quarterObject[q].end}" ORDER BY updated  ASC`;
-  window.open(`https://jira.atlightspeed.net/issues/?jql=${encodeURIComponent(query)}`)
+  return null;
 }
 
-const button = document.querySelector('[data-button]');
-button.addEventListener('click', () => {
-  checkLastQuarterProjects()
-})
+const textEl = document.querySelector('[data-text]');
+const quarterObj = getCurrentQuarter();
+textEl && quarterObj && (textEl.innerHTML = `
+<p>This is the </p>
+<h2>${quarterObj.quarter}</h2> 
+<p>it starts on <b>${quarterObj.startDate}</b></p> 
+<p>and ends on <b>${quarterObj.endDate}</b>.</p>
+<br>
+<p>Today is the <b>${quarterObj.todayDate}</b></p>
+`);
