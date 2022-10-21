9d6933bfa380c0b712f373f10bd91b6cc230a284
charles.hanna <charles.hanna@lightspeedhq.com>
2022-10-21 18:01:52 -0400

    few amendments to q_

--- which-quarter-is-it/script.js
+++ which-quarter-is-it/script.js
@@ -7,7 +7,7 @@ function getCurrentQuarter(){
   const quarterObject = {q1, q2, q3, q4};
 
   const date = new Date();
-  const fullDate = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`;
+  const fullDate = `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`;
   const currentTimestamp = new Date(fullDate).getTime();
   const quarterMap = {
     q1: 'First Quarter',
@@ -23,20 +23,40 @@ function getCurrentQuarter(){
         quarter: quarterMap[key],
         startDate: value.start,
         endDate: value.end,
-        todayDate: fullDate
+        todayDate: fullDate,
+        fiscalYear: quarterObject.q1 >= fullDate ? date.getFullYear()+1 : date.getFullYear()
       }
     }
   }
   return null;
 }
 
-const textEl = document.querySelector('[data-text]');
+
 const quarterObj = getCurrentQuarter();
+
+const monthesMap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const currentMonth = quarterObj.todayDate.split('/')[1];
+const currentDay = quarterObj.todayDate.split('/')[2];
+const daysInCurrentMonth = monthesMap[currentMonth -1];
+const remainingDaysInCurrentMonth = daysInCurrentMonth - currentDay;
+const currentQuarterStartMonth = quarterObj.startDate.split('/')[1];
+const daysInCurrentQuarter =  monthesMap[currentQuarterStartMonth-1] + monthesMap[(currentQuarterStartMonth-1)+1] + monthesMap[((currentQuarterStartMonth-1)+1)+1]
+// let daysInCurrentQuarter = 0;
+// for(let i = 0; i <= 3; i++){
+//   daysInCurrentQuarter += monthesMap[(currentQuarterStartMonth-1)+i]
+// }
+const remainingDaysTillEndOfQuarter = daysInCurrentQuarter - remainingDaysInCurrentMonth;
+
+
+const textEl = document.querySelector('[data-text]');
 textEl && quarterObj && (textEl.innerHTML = `
 <p>This is the </p>
-<h2>${quarterObj.quarter}</h2> 
+<h2>${quarterObj.quarter} of the Fiscal Year ${quarterObj.fiscalYear}</h2> 
 <p>it starts on <b>${quarterObj.startDate}</b></p> 
 <p>and ends on <b>${quarterObj.endDate}</b>.</p>
+<br>
+<h1 style="color:#FA00FF">${remainingDaysTillEndOfQuarter} days </h1>
+<p>remaining until end of <br>${quarterObj.quarter}</p>
 <br>
 <p>Today is the <b>${quarterObj.todayDate}</b></p>
 `);
